0d65968c23a64087614bb8e6391a7daecccaefcc
Carla Holcomb <carla.programmer@gmail.com>
2020-01-27 01:44:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
-    text_from_user = params[:user_text]
-
-    erb :results
+   
+   @analyzed_text = TextAnalyzer.new(params[:user_text])
+ 
+   erb :results
   end
+
 end
